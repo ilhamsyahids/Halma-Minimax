7ecca6913dbaef905fcc62c149abb585abfc9b65
Taufiq Husada Daryanto <taufiq.daryanto@xtremax.com>
2020-10-17 10:54:39 +0700

    combine with local search

--- algoritma.py
+++ algoritma.py
@@ -2,7 +2,7 @@ from math import sqrt
 
 cnt = [0, 0, 0, 0]
 
-def find_next_move(board, player_on_move):
+def find_next_move(board, player_on_move, using_local_search=True):
     '''
     input 
     board : array 2 dimensi, yang cell nya bernilai 0/1/2 
@@ -12,10 +12,10 @@ def find_next_move(board, player_on_move):
     best_move: ((Xawal, Yawal), (Xakhir, Yakhir))
     '''
 
-    best_val, best_move = minimax(0, float("-inf"), float("inf"), True, player_on_move, board)    
+    best_val, best_move = minimax(0, float("-inf"), float("inf"), True, player_on_move, board, using_local_search)    
     return best_move 
     
-def minimax(depth, alpha, beta, is_max, player_on_move, board):
+def minimax(depth, alpha, beta, is_max, player_on_move, board, using_local_search=True):
     cnt[depth]+=1
     # basis    
     if (depth==3):
@@ -29,15 +29,21 @@ def minimax(depth, alpha, beta, is_max, player_on_move, board):
     for i in range(len_board):
         for j in range(len_board):
             if (board[i][j]==player_on_move): # pawn milik player
-                list_possible_moves = find_possible_moves(board, player_on_move, i, j)
-
+                list_possible_moves = find_possible_moves(board, player_on_move, i, j, using_local_search)
+                
+                if (len(list_possible_moves)==0): # pionnya gabisa gerak
+                    continue
+                
                 for move in list_possible_moves:
+                    if (move == None):  # jaga jaga misal ada yang move nya none
+                        continue
+
                     # jalanin move nya
                     board[move[0]][move[1]] = player_on_move
                     board[i][j] = 0
                 
                     # hitung value minimax
-                    val, _ = minimax(depth+1, alpha, beta, not is_max, player_on_move ^ 3, board)
+                    val, _ = minimax(depth+1, alpha, beta, not is_max, player_on_move ^ 3, board, using_local_search)
 
                     # undo movenya
                     board[move[0]][move[1]] = 0
@@ -61,27 +67,54 @@ def minimax(depth, alpha, beta, is_max, player_on_move, board):
 
     return best_val, best_move
 
-def find_possible_moves(board, player_on_move, i, j):
+def find_possible_moves(board, player_on_move, i, j, using_local_search=True):
     x = []
     y = []
     if (player_on_move==1): # hanya milih gerakan maju ke arah lawan
         x = [1, 1, 0]
         y = [1, 0, 1]
-    else: # kalau player dua milih gerakan ke bawah
+    else:
         x = [-1, -1, 0]
         y = [-1, 0, -1]
 
+    len_board = len(board)
     list_possible_moves = []
     for k in range(3):
-        if (is_coor_valid(i + x[k], j + y[k], len(board))):
+        if (is_coor_valid(i + x[k], j + y[k], len_board)):
             if (board[i + x[k]][j + y[k]]==0):
                 list_possible_moves.append((i + x[k], j + y[k]))
-            else:    # jika cell depannya ga kosong, coba lompat 1 kali
-                if (is_coor_valid(i + 2*x[k], j + 2*y[k], len(board))):
-                    if (board[i + 2*x[k]][j + 2*y[k]]==0):
-                        list_possible_moves.append((i + 2*x[k], j + 2*y[k]))
+            else:    # jika cell depannya ga kosong, coba lompat
+                try_to_jump((i,j), board, x, y, list_possible_moves)
+                 
+
+    if (using_local_search): # kalau pake local search, tiap pawn cuma pilih 1 langkah terbaik (terdekat dengan goal)
+        best_move = None
+        best_val = float("inf") 
+        goal = (len_board-1, len_board-1) if player_on_move==1 else (0, 0)
+        for move in list_possible_moves:
+            val = find_distance(move, goal)
+            if (val < best_val):
+                best_move = move
+                best_val = val
+
+        list_possible_moves = [best_move]
 
     return list_possible_moves
+
+def try_to_jump(pos_now, board, x, y, list_possible_moves):
+    len_board = len(board)
+    ada_lompatan = False
+    for k2 in range(3):
+        if ((is_coor_valid(pos_now[0] + x[k2], pos_now[1] + y[k2], len_board)) and (board[pos_now[0] + x[k2]][pos_now[1] + y[k2]]!=0)):
+            if ((is_coor_valid(pos_now[0] + 2*x[k2], pos_now[1] + 2*y[k2], len_board)) and (board[pos_now[0] + 2*x[k2]][pos_now[1] + 2*y[k2]]==0)):
+                ada_lompatan = True
+                list_possible_moves.append((pos_now[0] + 2*x[k2], pos_now[1] + 2*y[k2]))
+                
+                pos_baru = (pos_now[0] + 2*x[k2], pos_now[1] + 2*y[k2])
+                try_to_jump(pos_baru, board, x, y, list_possible_moves)
+    
+    if(not ada_lompatan):
+        return
 
 def is_coor_valid(i, j, len_board):
     return (i >= 0 and i < len_board and j >= 0 and j < len_board)
@@ -107,18 +140,18 @@ def find_distance(p1, p2):
 
 
 if __name__=="__main__":
+    # ini buat nyoba aja
     board = [[1,1,1,1,0,0,0,0],
              [1,1,1,0,0,0,0,0],
              [1,1,0,0,0,0,0,0],
              [1,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,2],
              [0,0,0,0,0,0,2,2],
-             [1,0,0,0,0,2,2,2],
-             [1,0,0,0,2,2,2,2]]
+             [0,0,0,0,0,2,2,2],
+             [0,0,0,0,2,2,2,2]]
 
-    print(find_next_move(board, 2))
+    print(find_next_move(board, 1, using_local_search=True))
     print(cnt)
 
 
-
-# ganti coor di utility function, sama di move
+# belum nge handle gerakan mundur, kemungkinan ga perlu mundur /?
